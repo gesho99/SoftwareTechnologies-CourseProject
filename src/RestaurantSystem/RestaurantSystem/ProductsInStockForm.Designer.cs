40d1f26d776a3aa437969939fd480d95f73ec8e5
Георги Донков <54442891+gesho99@users.noreply.github.com>
2020-11-27 16:53:57 +0200

    Update ProductsInStockForm.Designer.cs
    #5

--- src/RestaurantSystem/RestaurantSystem/ProductsInStockForm.Designer.cs
+++ src/RestaurantSystem/RestaurantSystem/ProductsInStockForm.Designer.cs
@@ -44,6 +44,7 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Наличност
@@ -128,7 +129,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(210, 579);
+            this.button1.Location = new System.Drawing.Point(123, 576);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 23);
             this.button1.TabIndex = 10;
@@ -196,11 +197,21 @@
             this.label10.TabIndex = 16;
             this.label10.Text = "*";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 576);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Редактирай продукт";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // ProductsInStockForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 632);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -242,5 +253,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
     }
 }
