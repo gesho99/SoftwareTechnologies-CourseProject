9c22ae1a532b9bbc959efd13ad0394945e8f7777
bonchobonev99 <74185584+bonchobonev99@users.noreply.github.com>
2020-12-03 20:21:29 +0200

    Add files via upload
    Форми за меню и доставки

---
+++ src/RestaurantSystem/RestaurantSystem/Deliveries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantSystem
+{
+    public partial class Deliveries : Form
+    {
+        public Deliveries()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeliveryQuantity_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
