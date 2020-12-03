438ad3fdc0ae474b16611f775a28b35d00fc4ed5
Георги Донков <54442891+gesho99@users.noreply.github.com>
2020-12-03 20:53:14 +0200

    adding ef 6, mvc architecture and product model

---
+++ src/RestaurantSystem/RestaurantSystem/Data/RestaurantDbContext.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace RestaurantSystem.Data
+{
+    class RestaurantDbContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
