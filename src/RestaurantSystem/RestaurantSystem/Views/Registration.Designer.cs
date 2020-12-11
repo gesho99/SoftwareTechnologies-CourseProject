21491e8bab10e6c1d1917bfab603023838a33a5a
yoanaangelova <yoana_a@mail.bg>
2020-12-11 13:16:38 +0200

    Registration form
    Added the initial view for the regisration form

---
+++ src/RestaurantSystem/RestaurantSystem/Views/Registration.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace RestaurantSystem.Views
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.empID = new System.Windows.Forms.Label();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.jobPosition = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.browseBtn);
+            this.panel1.Controls.Add(this.jobPosition);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.emailBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lastName);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.firstName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.idBox);
+            this.panel1.Controls.Add(this.empID);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(382, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(312, 287);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(347, 325);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // empID
+            // 
+            this.empID.AutoSize = true;
+            this.empID.Location = new System.Drawing.Point(128, 17);
+            this.empID.Name = "empID";
+            this.empID.Size = new System.Drawing.Size(18, 13);
+            this.empID.TabIndex = 1;
+            this.empID.Text = "ID";
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(194, 17);
+            this.idBox.Name = "idBox";
+            this.idBox.ReadOnly = true;
+            this.idBox.Size = new System.Drawing.Size(100, 20);
+            this.idBox.TabIndex = 2;
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(194, 43);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(100, 20);
+            this.firstName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Име";
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(194, 69);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(100, 20);
+            this.lastName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Фамилия";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(194, 95);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(100, 20);
+            this.emailBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Имейл";
+            // 
+            // jobPosition
+            // 
+            this.jobPosition.Location = new System.Drawing.Point(194, 121);
+            this.jobPosition.Name = "jobPosition";
+            this.jobPosition.Size = new System.Drawing.Size(100, 20);
+            this.jobPosition.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Длъжност";
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(21, 132);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 11;
+            this.browseBtn.Text = "Избери";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.Location = new System.Drawing.Point(416, 319);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(96, 32);
+            this.registerBtn.TabIndex = 12;
+            this.registerBtn.Text = "Регистрирай";
+            this.registerBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(576, 319);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(96, 32);
+            this.cancelBtn.TabIndex = 13;
+            this.cancelBtn.Text = "Отказ";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 394);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox jobPosition;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.Label empID;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
