c7bc4ef3299c0180d3afc542508e18e97959b7cd
Георги Донков <54442891+gesho99@users.noreply.github.com>
2021-01-06 16:13:08 +0200

    adding LabelControllerTests, ListBoxControllerTests
    #25

---
+++ src/RestaurantSystem/RestaurantSystem.UnitTests/LabelControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using RestaurantSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xunit;
+
+namespace RestaurantSystem.UnitTests
+{
+    public class LabelControllerTests
+    {
+        Label label = new Label();
+
+        [Fact]
+        public void ChangeLabelVisibility_ShouldChangeToTrue()
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            bool actual = LabelController.ChangeLabelVisibility(ref label, true);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ChangeLabelVisibility_ShouldChangeToFalse()
+        {
+            //Arrange
+            bool expected = false;
+
+            //Act
+            bool actual = LabelController.ChangeLabelVisibility(ref label, false);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ChangeLabelText_ShouldChangeText()
+        {
+            //Arrange
+            string expected = "label text changed";
+
+            //Act
+            string actual = LabelController.ChangeLabelText(ref label, "label text changed");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ src/RestaurantSystem/RestaurantSystem.UnitTests/ListBoxControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using RestaurantSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xunit;
+
+namespace RestaurantSystem.UnitTests
+{
+    public class ListBoxControllerTests
+    {
+
+        ListBox listbox = new ListBox();
+
+        [Fact]
+        public void AddItemsInListBox_ShouldAddProduct()
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            ListBoxController.AddListBoxItems(ref listbox, "tomatoes");
+            bool actual = listbox.Items.Contains("tomatoes");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AddParametersInListBox_ShouldAddParameters_FirstTest()
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            ListBoxController.AddListBoxParameters(ref listbox, 10, 5.60, 5.60);
+            bool actual = listbox.Items.Contains(10 + " " + 5.60 + " " + 5.60);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AddParametersInListBox_ShouldAddParameters_SecondTest()
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            ListBoxController.AddListBoxParameters(ref listbox, 5.60, "test", 5.60);
+            bool actual = listbox.Items.Contains(5.60 + " test " + 5.60);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ClearItemsInListBox_ShouldClearItems()
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            ListBoxController.ClearItems(ref listbox);
+            bool actual = listbox.Items.Count == 0;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
